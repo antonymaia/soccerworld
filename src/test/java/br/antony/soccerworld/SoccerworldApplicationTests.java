b0ac3bb15258c23d8c52f47a2815780bfad17480
Antony Maia <antonymaiati@gmail.com>
2021-11-30 19:24:10 -0300

    First Commit

---
+++ src/test/java/br/antony/soccerworld/SoccerworldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.antony.soccerworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
